fc648e46074b080620a2d6a8c44379702950fb0c
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-01 22:06:12 +0200

    After creating the crm_cust_info table

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,10 @@
+-- Create the 'cust_info' table
+CREATE TABLE bronze.crm_cust_info(
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date DATE
+);
