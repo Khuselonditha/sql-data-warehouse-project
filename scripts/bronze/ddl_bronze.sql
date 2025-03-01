f430b66587ba2307b0fabbbef95960b197e7ab1b
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-01 23:20:24 +0200

    After adding checks if tables exist before creating them

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,5 +1,11 @@
+
+-- Check if table 'bronze.crm_cust_info' exists before creation
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
+
 -- Create the 'cust_info' table
-CREATE TABLE bronze.crm_cust_info(
+CREATE TABLE bronze.crm_cust_info (
     cst_id INT,
     cst_key NVARCHAR(50),
     cst_firstname NVARCHAR(50),
@@ -10,8 +16,12 @@ CREATE TABLE bronze.crm_cust_info(
 );
 GO
 
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
+
 -- Create the 'prd_info' table
-CREATE TABLE bronze.crm_prd_info(
+CREATE TABLE bronze.crm_prd_info (
     prd_id INT,
     prd_key NVARCHAR(50),
     prd_nm NVARCHAR(50),
@@ -22,8 +32,13 @@ CREATE TABLE bronze.crm_prd_info(
 );
 GO
 
+
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
+
 -- Create the 'sales_details' table
-CREATE TABLE bronze.crm_sales_details(
+CREATE TABLE bronze.crm_sales_details (
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
     sls_cust_id INT,
@@ -36,23 +51,35 @@ CREATE TABLE bronze.crm_sales_details(
 );
 GO
 
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
 -- Create the 'CUST_AZ12' table
-CREATE TABLE bronze.erp_cust_az12(
+CREATE TABLE bronze.erp_cust_az12 (
     cid NVARCHAR(50),
     bdate DATE,
     gen NVARCHAR(50)
 );
 GO
 
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
+
 -- Create the 'LOC_A101' table
-CREATE TABLE bronze.erp_loc_a101(
+CREATE TABLE bronze.erp_loc_a101 (
     cid NVARCHAR(50),
     cntry NVARCHAR(50)
 );
 GO
 
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
 -- Create the 'PX_CAT_G1V2' table
-CREATE TABLE bronze.erp_px_cat_g1v2(
+CREATE TABLE bronze.erp_px_cat_g1v2 (
     id NVARCHAR(50),
     cat NVARCHAR(50),
     subcat NVARCHAR(50),
