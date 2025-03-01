bff8f6efdcba74a6d912d735d31db7ca9ec3bc56
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-01 22:37:18 +0200

    After creating the crm_prd_info table

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -8,3 +8,15 @@ CREATE TABLE bronze.crm_cust_info(
     cst_gndr NVARCHAR(50),
     cst_create_date DATE
 );
+GO
+
+-- Create the 'prd_info' table
+CREATE TABLE bronze.prd_info(
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE,
+);
