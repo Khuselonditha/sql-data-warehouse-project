10d14b59824603845924840ccebd89ff11e74c5d
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-01 22:58:28 +0200

    After creating the erp_px_cat_g1v2 table

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -50,3 +50,11 @@ CREATE TABLE bronze.erp_loc_a101(
     cntry NVARCHAR(50)
 );
 GO
+
+-- Create the 'PX_CAT_G1V2' table
+CREATE TABLE bronze.erp_px_cat_g1v2(
+    id NVARCHAR(50),
+    cat NVARCHAR(50),
+    subcat NVARCHAR(50),
+    maintainance NVARCHAR(50)
+);
