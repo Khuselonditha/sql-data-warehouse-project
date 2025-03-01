f6b2331ba78ddb82d431d1600df369f1711e724f
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-01 22:53:58 +0200

    After creating the erp_loc_a101 table

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -42,3 +42,11 @@ CREATE TABLE bronze.erp_cust_az12(
     bdate DATE,
     gen NVARCHAR(50)
 );
+GO
+
+-- Create the 'LOC_A101' table
+CREATE TABLE bronze.erp_loc_a101(
+    cid NVARCHAR(50),
+    cntry NVARCHAR(50)
+);
+GO
