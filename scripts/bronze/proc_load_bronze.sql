0a72cbfe87c3174a81493625f440c5ba8ea0640f
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-02 21:12:35 +0200

    After fixing the path error for the bronze.crm_cust_info table

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,6 +1,6 @@
 -- Insert data to the 'bronze.crm_cust_info' table
 BULK INSERT bronze.crm_cust_info
-FROM '/home/khuselo/my_work/side_things/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+FROM "Z:\home\khuselo\my_work\side_things\sql-data-warehouse-project\datasets\source_crm\cust_info.csv"
 WITH (
     FIRST_ROW = 2,
     FIELDTERMINATOR = ',',
