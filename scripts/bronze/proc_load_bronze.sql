82cfc4f9e7ccc947e50315bc9040f6d86f65e07a
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-02 21:10:23 +0200

    After creating a bulk insert for the bronze.crm_cust_info table

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,8 @@
+-- Insert data to the 'bronze.crm_cust_info' table
+BULK INSERT bronze.crm_cust_info
+FROM '/home/khuselo/my_work/side_things/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+WITH (
+    FIRST_ROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
