1c42c685cbd9541464804a6a52b23af6787db00c
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-03 17:01:23 +0200

    After adding logging/prints to our stored procedure code

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,9 +1,17 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-    -- Delete/truncate all rows of the 'bronze.crm_cust_info' table
+    PRINT '======================================================================';
+    PRINT 'Loading Bronze Layer';
+    PRINT '======================================================================';
+    
+    PRINT '----------------------------------------------------------------------';
+    PRINT 'Loading CRM Tables'
+    PRINT '----------------------------------------------------------------------';
+
+    PRINT '>> Truncating Table: bronze.crm_cust_info'
     TRUNCATE TABLE bronze.crm_cust_info;
 
-    -- Insert data to the 'bronze.crm_cust_info' table
+    PRINT '>> Inserting Data Into: bronze.crm_cust_info'
     BULK INSERT bronze.crm_cust_info
     FROM "Z:\home\khuselo\my_work\side_things\sql-data-warehouse-project\datasets\source_crm\cust_info.csv"
     WITH (
@@ -12,10 +20,10 @@ BEGIN
         TABLOCK
     );
 
-    -- Delete/truncate all rows of the 'bronze.crm_prd_info' table
+    PRINT '>> Truncating Table: bronze.crm_prd_info'
     TRUNCATE TABLE bronze.crm_prd_info;
 
-    -- Insert data to the 'bronze.crm_prd_info' table
+    PRINT '>> Inserting Data Into: bronze.crm_prd_info'
     BULK INSERT bronze.crm_prd_info
     FROM "Z:\home\khuselo\my_work\side_things\sql-data-warehouse-project\datasets\source_crm\prd_info.csv"
     WITH (
@@ -24,10 +32,10 @@ BEGIN
         TABLOCK
     );
 
-    -- Delete/truncate all rows of the 'bronze.crm_sales_details' table
+    PRINT '>> Truncating Table: bronze.crm_sales_details'
     TRUNCATE TABLE bronze.crm_sales_details;
 
-    -- Insert data to the 'bronze.crm_sales_details' table
+    PRINT '>> Inserting Data Into: bronze.crm_sales_details'
     BULK INSERT bronze.crm_sales_details
     FROM "Z:\home\khuselo\my_work\side_things\sql-data-warehouse-project\datasets\source_crm\sales_details.csv"
     WITH (
@@ -36,11 +44,14 @@ BEGIN
         TABLOCK
     );
 
+    PRINT '----------------------------------------------------------------------';
+    PRINT 'Loading ERP Tables'
+    PRINT '----------------------------------------------------------------------';
 
-    -- Delete/truncate all rows of the 'bronze.erp_cust_az12' table
+    PRINT '>> Truncating Table: bronze.erp_cust_az12'
     TRUNCATE TABLE bronze.erp_cust_az12;
 
-    -- Insert data to the 'bronze.erp_cust_az12' table
+    PRINT '>> Inserting Data Into: bronze.erp_cust_az12'
     BULK INSERT bronze.erp_cust_az12
     FROM "Z:\home\khuselo\my_work\side_things\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv"
     WITH (
@@ -49,10 +60,10 @@ BEGIN
         TABLOCK
     );
 
-    -- Delete/truncate all rows of the 'bronze.erp_loc_a101' table
+    PRINT '>> Truncating Table: bronze.erp_loc_a101'
     TRUNCATE TABLE bronze.erp_loc_a101;
 
-    -- Insert data to the 'bronze.erp_loc_a101' table
+    PRINT '>> Inserting Data Into: bronze.erp_cust_az12'
     BULK INSERT bronze.erp_loc_a101
     FROM "Z:\home\khuselo\my_work\side_things\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv"
     WITH (
@@ -61,11 +72,10 @@ BEGIN
         TABLOCK
     );
 
-    -- Delete/truncate all rows of the 'bronze.erp_px_cat_g1v2' table
+    PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2'
     TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
-
-    -- Insert data to the 'bronze.px_cat_g1v2' table
+    PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2'
     BULK INSERT bronze.erp_px_cat_g1v2
     FROM "Z:\home\khuselo\my_work\side_things\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv"
     WITH (
