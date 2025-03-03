bc3660492c9861516f9e131608d34234b160a9a8
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-03 15:53:30 +0200

    After making sure we clear the bronze.crm_sales_details table before inserting data

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -22,6 +22,9 @@ WITH (
     TABLOCK
 );
 
+-- Delete/truncate all rows of the 'bronze.crm_sales_details' table
+TRUNCATE TABLE bronze.crm_sales_details;
+
 -- Insert data to the 'bronze.crm_sales_details' table
 BULK INSERT bronze.crm_sales_details
 FROM "Z:\home\khuselo\my_work\side_things\sql-data-warehouse-project\datasets\source_crm\sales_details.csv"
