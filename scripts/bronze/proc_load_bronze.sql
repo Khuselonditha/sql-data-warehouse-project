eb091b034601c947af660a85ffb3aefd5811e65b
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-03 15:45:03 +0200

    After making sure we clear the bronze.crm_cust_info table before inserting data

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,3 +1,6 @@
+-- Delete/truncate all rows of the 'bronze.crm_cust_info' table
+TRUNCATE TABLE bronze.crm_cust_info;
+
 -- Insert data to the 'bronze.crm_cust_info' table
 BULK INSERT bronze.crm_cust_info
 FROM "Z:\home\khuselo\my_work\side_things\sql-data-warehouse-project\datasets\source_crm\cust_info.csv"
