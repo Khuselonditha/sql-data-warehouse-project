4244583fc527df002a8edae70e7b9cabf34fbdde
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-06 18:33:00 +0200

    After creating a column for metadata to all our silver layer tables

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -11,7 +11,8 @@ CREATE TABLE silver.crm_cust_info (
     cst_lastname NVARCHAR(50),
     cst_marital_status NVARCHAR(50),
     cst_gndr NVARCHAR(50),
-    cst_create_date DATE
+    cst_create_date DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -27,7 +28,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_cost INT,
     prd_line NVARCHAR(50),
     prd_start_dt DATE,
-    prd_end_dt DATE
+    prd_end_dt DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -46,7 +48,8 @@ CREATE TABLE silver.crm_sales_details (
     sls_due_dt INT,
     sls_sales INT,
     sls_quantity INT,
-    sls_price INT
+    sls_price INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -58,7 +61,8 @@ GO
 CREATE TABLE silver.erp_cust_az12 (
     cid NVARCHAR(50),
     bdate DATE,
-    gen NVARCHAR(50)
+    gen NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -69,7 +73,8 @@ GO
 -- Create the 'LOC_A101' table
 CREATE TABLE silver.erp_loc_a101 (
     cid NVARCHAR(50),
-    cntry NVARCHAR(50)
+    cntry NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -82,6 +87,7 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     id NVARCHAR(50),
     cat NVARCHAR(50),
     subcat NVARCHAR(50),
-    maintainance NVARCHAR(50)
+    maintainance NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
