1eb01fa95cd5f4d4803acc691ab44df11fe8bf8c
Khuselo Nditha <khuselonditha@gmail.com>
2025-03-06 18:20:47 +0200

    Creating tables after checking if they are empty.

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,87 @@
+-- Check if table 'silver.crm_cust_info' exists before creation
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_cust_info;
+GO
+
+-- Create the 'cust_info' table
+CREATE TABLE silver.crm_cust_info (
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date DATE
+);
+GO
+
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
+GO
+
+-- Create the 'prd_info' table
+CREATE TABLE silver.crm_prd_info (
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE
+);
+GO
+
+
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
+GO
+
+-- Create the 'sales_details' table
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
+);
+GO
+
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
+GO
+
+-- Create the 'CUST_AZ12' table
+CREATE TABLE silver.erp_cust_az12 (
+    cid NVARCHAR(50),
+    bdate DATE,
+    gen NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
+GO
+
+-- Create the 'LOC_A101' table
+CREATE TABLE silver.erp_loc_a101 (
+    cid NVARCHAR(50),
+    cntry NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
+GO
+
+-- Create the 'PX_CAT_G1V2' table
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id NVARCHAR(50),
+    cat NVARCHAR(50),
+    subcat NVARCHAR(50),
+    maintainance NVARCHAR(50)
+);
+GO
